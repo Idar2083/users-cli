4c30099b8e268b5f6433a29906d4c1f94197f111
Idar Tsymzhitov <jihannaxz1@gmail.com>
2026-05-04 23:07:26 +0700

    Fix repository and update functionality for HTTP API

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
-require_once __DIR__ . "../vendor/autoload.php";
+require_once __DIR__ . "/../vendor/autoload.php";
+require_once __DIR__ . '/../src/RepositoryFactory.php';
 
 use function Tsimib\UsersCli\createRepository;
 
